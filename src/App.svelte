f60c9f7cdada12e6c98562150e021d2e912f9221
zeddshaw <zaygo@mail.com>
2020-08-07 13:41:14 +0530

    Design created

---
+++ src/App.svelte
@@ -0,0 +1,132 @@
+<script>
+	
+</script>
+
+<style>
+	
+</style>
+
+<section>
+        <div class="container">
+            <div class="row mt-4">
+                <div class="col col-md-12 col-12 text-white">
+                    <div class="input-group sticky"><input type="search" id="searchbar-dark" class="form-control shadow-none" placeholder="Search">
+                        <div class="input-group-prepend"><button class="btn btn-primary" type="button" id="searchbtn-dark"><i class="fa fa-search"></i></button></div>
+                    </div>
+                </div>
+            </div>
+			<!-- first stat row -->
+            <div class="row mt-4">
+                <div class="col col-md-4 col-12 text-white mt-2">
+                    <div class="card" id="tile-1-dark">
+                        <div class="card-body">
+                            <h1 style="font-weight: 800;">120</h1>
+                            <p class="d-xl-flex align-items-xl-center"><i class="far fa-user" style="font-size: 20px;"></i>&nbsp;Registered</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-md-4 col-12 text-black mt-2">
+                    <div class="card" id="tile-2-dark">
+                        <div class="card-body text-white">
+                            <h1 style="font-weight: 800;">40</h1>
+                            <p class="d-xl-flex align-items-xl-center"><i class="material-icons" style="font-size: 20px;">fingerprint</i>&nbsp;Logged In</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-md-4 col-12 text-black mt-2">
+                    <div class="card" id="tile-3-dark">
+                        <div class="card-body text-white">
+                            <h1 style="font-weight: 800;">5</h1>
+                            <p class="d-xl-flex align-items-xl-center"><i class="material-icons" style="font-size: 20px;">block</i>&nbsp;Disabled</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+			<!-- second stat row -->
+			<div class="row mt-4">
+                <div class="col col-md-4 col-12 text-white mt-2">
+                    <div class="card" id="tile-4-dark">
+                        <div class="card-body">
+                            <h1 style="font-weight: 800;">25.3 %</h1>
+                            <p class="d-xl-flex align-items-xl-center"><i class="material-icons" style="font-size: 20px;">graphic_eq</i>&nbsp;CPU</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-md-4 col-12 text-black mt-2">
+                    <div class="card" id="tile-5-dark">
+                        <div class="card-body text-white">
+                            <h1 style="font-weight: 800;">43.7 %</h1>
+                            <p class="d-xl-flex align-items-xl-center"><i class="material-icons" style="font-size: 20px;">memory</i>&nbsp;Memory</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-md-4 col-12 text-black mt-2">
+                    <div class="card" id="tile-6-dark">
+                        <div class="card-body text-white">
+                            <h1 style="font-weight: 800;">10 %</h1>
+                            <p class="d-xl-flex align-items-xl-center"><i class="material-icons" style="font-size: 20px;">dns</i>&nbsp;Disk</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+			<!-- end of stats -->
+            <div class="row mt-5">
+                <div class="col col-md-6 col-12 mt-2">
+                    <div class="card" id="tel-card-dark">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-weight: bold;">Arjun Nair</h5><label>NAME</label></div>
+                    </div>
+                </div>
+                <div class="col col-md-6 col-12 mt-2">
+                    <div class="card" id="tel-card-dark">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-weight: bold;">zaygo</h5><label>USERNAME</label></div>
+                    </div>
+                </div>
+                <div class="col col-md-6 col-12 mt-2">
+                    <div class="card" id="tel-card-dark">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-weight: bold;">karmarex@pm.me</h5><label>EMAIL</label></div>
+                    </div>
+                </div>
+                <div class="col col-md-6 col-12 mt-2">
+                    <div class="card" id="tel-card-dark">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-weight: bold;">user : premium</h5><label>SCOPE</label></div>
+                    </div>
+                </div>
+                <div class="col col-md-6 col-12 mt-2">
+                    <div class="card" id="tel-card-dark">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-weight: bold;">403, Samarth Apt, Katraj, Pune 411046</h5><label>ADDRESS</label></div>
+                    </div>
+                </div>
+				<div class="col col-md-6 col-12 mt-2">
+                    <div class="card" id="tel-card-dark">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-weight: bold;">India</h5><label>COUNTRY</label></div>
+                    </div>
+                </div>
+				<div class="col col-md-6 col-12 mt-2">
+                    <div class="card" id="tel-card-dark">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-weight: bold;">No</h5><label>DISABLED</label></div>
+                    </div>
+                </div>
+				<div class="col col-md-6 col-12 mt-2">
+                    <div class="card" id="tel-card-dark">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-weight: bold;">Zagan</h5><label>APPLICATION</label></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col" align="center">
+				<button class="btn btn-primary mr-2" type="button" id="boxbutton">Update</button>
+				<button class="btn btn-primary mr-2" type="button" id="boxbutton2">Disable</button>
+				<button class="btn btn-primary" type="button" id="boxbutton3">Terminate</button>
+				</div>
+            </div>
+            <br>
+        </div>
+</section>
